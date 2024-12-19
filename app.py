2653fe39ab1b47148c17516e65726811a8457d11
black-lemonov <egor.porf1@mail.ru>
2024-12-19 20:57:44 +0300

    исправлено окно с запросами

--- app.py
+++ app.py
@@ -1,143 +1,137 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import scrolledtext, messagebox
+import tkinter.scrolledtext as st
+import tkinter.messagebox as msg
 
 from rdflib import Graph
 
 
 class App:
-    GRAPH_IRI: str = "/home/egorp/Учеба/Модели ИС/Охрана_природы_исправленная.rdf"
+    GRAPH_IRI: str = "./resources/ecology.rdf"
+    PREFIXES: tuple[str] = (
+        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
+        "PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>"    
+    )
+    QUERIES = {
+        "SELECT" : """
+        SELECT ?название_материала ?автор ?тема
+            WHERE {
+                ?материал ont:Название_материала ?название_материала .
+                ?материал ont:Материал_Активиста ?активист .
+                ?активист ont:ФИО_активиста ?автор .
+                ?материал ont:Материал_о_Проблеме ?проблема .
+                ?проблема ont:Направленность_деятельности ?тема
+            }
+        """,
+        "CONSTRUCT" : """
+        CONSTRUCT {
+            ?материал ont:Название_материала ?название .
+            ?материал ont:Материал_активиста ?автор .
+        }
+            WHERE {
+                ?материал ont:Название_материала ?название .
+                ?материал ont:Материал_активиста ?автор .
+            }
+        """,
+        "ASK" : """
+        ASK {
+            ?активист ont:ФИО_активиста "Кубанин Андрей Анатольевич" .
+        }
+        """
+    }
     
     def __init__(self):
-        self._set_graph()
-        self._set_root()
-        self._set_output_field()
-        self._set_input_field()
-        self._set_buttons()
-
-    def _set_graph(self) -> None:
-        '''Создание и загрузка графа RDF'''
-        self.__graph = Graph()
-        self.__graph.parse(self.GRAPH_IRI)  # Пример загрузки данных из URL
-
-    def _set_root(self) -> None:
-        '''Создание главного окна'''
-        self.__root = tk.Tk()
-        self.__root.title("Охрана Природы")
+        self.graph = Graph()
+        self.graph.parse(self.GRAPH_IRI)
+        
+        self.root = tk.Tk()
+        self.root.title("Охрана Природы")
+        
+        self.set_query_field()
     
-    def _set_output_field(self) -> None:
-        '''Создание поля для вывода результатов запроса'''
-        ttk.Label(
-            self.__root,
-            text="Результат",
-        ).pack(
-            side="top",
-            expand=True,
-            fill='x',
-            pady=10,
-            padx=10
-        )
-        self.__result_output = scrolledtext.ScrolledText(self.__root, width=60, height=10)
-        self.__result_output.pack(
-            side='top',
-            expand=True,
-            fill="both",
-            pady=10,
-            padx=10
-        )
+        self.set_predefined()
         
-    def _set_input_field(self) -> None:
-        '''Создание поля для ввода пользовательского SPARQL запроса'''
-        ttk.Label(
-            self.__root,
-            text="Запрос",
-        ).pack(
-            side="top",
-            fill="x",
-            pady=10
-        )
-        self.__query_input = scrolledtext.ScrolledText(self.__root, width=60, height=10)
-        self.__query_input.pack(
-            side='top',
-            fill="both",
-            expand=True,
-            pady=10,
-            padx=10
-        )
+    def set_query_field(self) -> None:
+        frame = ttk.Frame(self.root)
+        frame.pack(side="top")
         
-    def _set_buttons(self) -> None:
-        '''Создание кнопок для выполнения запросов'''
-        predefined_queries: dict[str, str] = {
-            "Активисты" : """
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX owl: <http://www.w3.org/2002/07/owl#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>
-            SELECT ?Активист
-	            WHERE {?Активист rdf:type ont:Activist}
-            """,
-            "Материалы" : """
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX owl: <http://www.w3.org/2002/07/owl#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>
-            SELECT ?материал ?название
-	            WHERE { ?материал ont:Название_материала ?название }
-            """,
-            "Законы" : """
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX owl: <http://www.w3.org/2002/07/owl#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>
-            SELECT ?закон ?проблема
-	            WHERE {?закон ont:Закон_о_Проблеме ?проблема}
-            """
-        }
+        self.query_field = st.ScrolledText(
+            frame, 
+            height=10
+        )
+        self.query_field.grid(row=0, column=1, rowspan=3, padx=10, pady=10)
         
-        for query_name, query in predefined_queries.items():
-            button = ttk.Button(self.__root, text=query_name, command=lambda q=query: self._run_predefined_query(q))
-            button.pack(
-                side='right'
-            )
-            
-        self.__execute_button = ttk.Button(
-            self.__root, 
+        ttk.Button(
+            frame, 
             text="Выполнить запрос",
-            command=self._run_custom_query
-        )
-        self.__execute_button.pack(
-            side='right'
-        )
+            command=lambda: self.execute_query(self.query_field.get("1.0", tk.END).strip())
+        ).grid(row=0, column=0, padx=10, pady=10)
+        
+        ttk.Button(
+            frame, 
+            text="Очистить поле",
+            command=self.clear_query
+        ).grid(row=1, column=0, padx=10, pady=10)
+        
+    
+    def clear_query(self) -> None:
+        self.query_field.delete("1.0", tk.END)
+        
+        
+    def set_predefined(self) -> None:
+        frame = ttk.Frame(self.root)
+        frame.pack(side="top")
+        
+        for title, query in self.QUERIES.items():
+            ttk.Button(
+                frame, 
+                text=title,
+                command=lambda q=query: self.query_field.insert('1.0', q.strip())
+            ).pack(side='left', padx=5, pady=5)
+        
+    def create_results_table(self, column_names: None, data: None) -> None:
+        result_win = tk.Tk()
+        result_win.title("Результаты запроса")
+        
+        tree = ttk.Treeview(result_win, columns=column_names, show='headings')
 
-    def _execute_query(self, query) -> str:
+        for col in column_names:
+            tree.heading(col, text=col)
+            tree.column(col, width=100)
+
+        for row in data:
+            tree.insert("", "end", values=row)
+
+        tree.pack(expand=True, fill='both')
+
+    def execute_query(self, query: str) -> None:
+        if not query:
+            msg.showwarning("Ошибка", "Поле запроса пустое")
+            return
         try:
-            results = self.__graph.query(query)
-            result_str = ""
+            results = self.graph.query(
+                self.add_prefixes(query)
+            )
+            parsed_data = [] 
             for row in results:
-                result_str += str(row) + "\n"
-            return result_str.strip()
+                parsed_data.append([row.get(var) for var in results.vars])
+            
+            self.create_results_table(
+                column_names=results.vars,
+                data=parsed_data
+            )
         except Exception as e:
-            return str(e)
-
-    def _run_custom_query(self):
-        query = self.__query_input.get("1.0", tk.END).strip()
-        if query:
-            result = self._execute_query(query)
-            self.__result_output.delete("1.0", tk.END)
-            self.__result_output.insert(tk.END, result)
-        else:
-            messagebox.showwarning("Warning", "Введите SPARQL запрос.")
-
-    def _run_predefined_query(self, query):
-        result = self._execute_query(query)
-        self.__result_output.delete("1.0", tk.END)
-        self.__result_output.insert(tk.END, result)
+            msg.showerror("Ошибка", str(e))
+            
+    def add_prefixes(self, query: str) -> str:
+        return '\n'.join(self.PREFIXES) + query
 
     def run(self) -> None:
-        self.__root.mainloop()
+        self.root.mainloop()
+
 
 
 if __name__ == "__main__":
