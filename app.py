ba1e2dc55fcffbc40668eb98182a5cc71738f25d
black-lemonov <egor.porf1@mail.ru>
2024-12-19 23:23:01 +0300

    добавлена библиотека ttkthemes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tkinter.ttk as ttk
 import tkinter.scrolledtext as st
 import tkinter.messagebox as msg
 
+from ttkthemes import ThemedTk
 from rdflib import Graph
 
 
@@ -13,7 +14,7 @@ class App:
         "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
         "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
         "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
-        "PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>"    
+        "PREFIX ont: <http://www.semanticweb.org/egorp/ontologies/2024/11/untitled-ontology-20#>"   # Возможно надо менять 
     )
     QUERIES = {
         "SELECT" : """
@@ -44,42 +45,52 @@ class App:
     }
     
     def __init__(self):
+        self.root = ThemedTk(theme="ubuntu")
+        self.root.title("Охрана Природы")
+        
         self.graph = Graph()
         self.graph.parse(self.GRAPH_IRI)
         
-        self.root = tk.Tk()
-        self.root.title("Охрана Природы")
-        
-        self.set_query_field()
-    
+        self.set_query_txt()
         self.set_predefined()
-        
-    def set_query_field(self) -> None:
+    
+    def set_query_txt(self) -> None:
         frame = ttk.Frame(self.root)
         frame.pack(side="top")
         
-        self.query_field = st.ScrolledText(
+        self.query_txt = st.ScrolledText(
             frame, 
             height=10
         )
-        self.query_field.grid(row=0, column=1, rowspan=3, padx=10, pady=10)
+        self.query_txt.grid(row=0, column=1, rowspan=3, padx=10, pady=10)
         
         ttk.Button(
             frame, 
             text="Выполнить запрос",
-            command=lambda: self.execute_query(self.query_field.get("1.0", tk.END).strip())
-        ).grid(row=0, column=0, padx=10, pady=10)
-        
-        ttk.Button(
-            frame, 
-            text="Очистить поле",
-            command=self.clear_query
+            command=lambda: self.execute_query(self.query_txt.get("1.0", tk.END).strip())
         ).grid(row=1, column=0, padx=10, pady=10)
         
-    
-    def clear_query(self) -> None:
-        self.query_field.delete("1.0", tk.END)
+        self.flag_var = tk.IntVar(value=1)
+        ttk.Checkbutton(frame, text="Включить префиксы", variable=self.flag_var).grid(row=2, column=0, padx=10, pady=10)
         
+    def execute_query(self, query: str) -> None:
+        if not query:
+            msg.showwarning("Ошибка", "Поле запроса пустое")
+            return
+        try:
+            results = self.graph.query(
+                self.add_prefixes(query) if self.flag_var.get() == 1 else query
+            )
+            parsed_data = [] 
+            for row in results:
+                parsed_data.append([row.get(var) for var in results.vars])
+            
+            self.create_results_table(
+                column_names=results.vars,
+                data=parsed_data
+            )
+        except Exception as e:
+            msg.showerror("Ошибка", str(e))
         
     def set_predefined(self) -> None:
         frame = ttk.Frame(self.root)
@@ -89,8 +100,17 @@ class App:
             ttk.Button(
                 frame, 
                 text=title,
-                command=lambda q=query: self.query_field.insert('1.0', q.strip())
+                command=lambda q=query: self.query_txt.insert('1.0', q.strip())
             ).pack(side='left', padx=5, pady=5)
+        
+        ttk.Button(
+            frame, 
+            text="Очистить",
+            command=self.clear_query
+        ).pack(side='left', padx=5, pady=5)
+        
+    def clear_query(self) -> None:
+        self.query_txt.delete("1.0", tk.END)
         
     def create_results_table(self, column_names: None, data: None) -> None:
         result_win = tk.Tk()
@@ -106,33 +126,15 @@ class App:
             tree.insert("", "end", values=row)
 
         tree.pack(expand=True, fill='both')
-
-    def execute_query(self, query: str) -> None:
-        if not query:
-            msg.showwarning("Ошибка", "Поле запроса пустое")
-            return
-        try:
-            results = self.graph.query(
-                self.add_prefixes(query)
-            )
-            parsed_data = [] 
-            for row in results:
-                parsed_data.append([row.get(var) for var in results.vars])
-            
-            self.create_results_table(
-                column_names=results.vars,
-                data=parsed_data
-            )
-        except Exception as e:
-            msg.showerror("Ошибка", str(e))
             
     def add_prefixes(self, query: str) -> str:
         return '\n'.join(self.PREFIXES) + query
+        
 
     def run(self) -> None:
         self.root.mainloop()
-
-
+    
+            
 
 if __name__ == "__main__":
     App().run()
