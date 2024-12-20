01d6ef38b83f293d1872a86d0a2c57642ef6c1c3
black-lemonov <egor.porf1@mail.ru>
2024-12-20 09:56:38 +0300

    добавлено отображение ASK запроса

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ class App:
         "CONSTRUCT" : """
         CONSTRUCT {
             ?материал ont:Название_материала ?название .
-            ?материал ont:Материал_активиста ?автор .
+            ?материал ont:Материал_Aктивиста ?автор .
         }
             WHERE {
                 ?материал ont:Название_материала ?название .
-                ?материал ont:Материал_активиста ?автор .
+                ?материал ont:Материал_Aктивиста ?автор .
             }
         """,
         "ASK" : """
@@ -62,7 +62,7 @@ class App:
             frame, 
             height=10
         )
-        self.query_txt.grid(row=0, column=1, rowspan=3, padx=10, pady=10)
+        self.query_txt.grid(row=0, column=1, rowspan=4, padx=10, pady=10)
         
         ttk.Button(
             frame, 
@@ -70,27 +70,15 @@ class App:
             command=lambda: self.execute_query(self.query_txt.get("1.0", tk.END).strip())
         ).grid(row=1, column=0, padx=10, pady=10)
         
-        self.flag_var = tk.IntVar(value=1)
-        ttk.Checkbutton(frame, text="Включить префиксы", variable=self.flag_var).grid(row=2, column=0, padx=10, pady=10)
+        self.table_check_var = tk.IntVar(value=1)
+        ttk.Checkbutton(
+            frame, text="В виде таблицы", variable=self.table_check_var
+        ).grid(row=2, column=0, padx=10, pady=10)
         
-    def execute_query(self, query: str) -> None:
-        if not query:
-            msg.showwarning("Ошибка", "Поле запроса пустое")
-            return
-        try:
-            results = self.graph.query(
-                self.add_prefixes(query) if self.flag_var.get() == 1 else query
-            )
-            parsed_data = [] 
-            for row in results:
-                parsed_data.append([row.get(var) for var in results.vars])
-            
-            self.create_results_table(
-                column_names=results.vars,
-                data=parsed_data
-            )
-        except Exception as e:
-            msg.showerror("Ошибка", str(e))
+        self.prefixes_check_var = tk.IntVar(value=0)
+        ttk.Checkbutton(
+            frame, text="Без префиксов", variable=self.prefixes_check_var
+        ).grid(row=3, column=0, padx=10, pady=10)
         
     def set_predefined(self) -> None:
         frame = ttk.Frame(self.root)
@@ -112,7 +100,41 @@ class App:
     def clear_query(self) -> None:
         self.query_txt.delete("1.0", tk.END)
         
-    def create_results_table(self, column_names: None, data: None) -> None:
+    def execute_query(self, query: str) -> None:
+        if not query:
+            msg.showwarning("Ошибка", "Поле запроса пустое")
+            return
+        try:
+            results = self.graph.query(
+                self.add_prefixes(query) if self.prefixes_check_var.get() == 0 else query
+            )
+            
+            if not results.vars:
+                if self.table_check_var.get() == 1:
+                    self.create_results_table(['ASK'], [[results.askAnswer]])
+                else:
+                    self.create_results_text([[results.askAnswer]])
+                return
+            
+            parsed_data = [] 
+            for row in results:
+                parsed_data.append([row.get(var) for var in results.vars])
+            
+            if self.table_check_var.get() == 1:
+                self.create_results_table(
+                    column_names=results.vars,
+                    data=parsed_data
+                )
+            else:
+                self.create_results_text(parsed_data)
+            
+        except Exception as e:
+            msg.showerror("Ошибка", str(e))
+    
+    def add_prefixes(self, query: str) -> str:
+        return '\n'.join(self.PREFIXES) + query
+        
+    def create_results_table(self, column_names: list[str], data = list[list]) -> None:
         result_win = tk.Tk()
         result_win.title("Результаты запроса")
         
@@ -126,15 +148,23 @@ class App:
             tree.insert("", "end", values=row)
 
         tree.pack(expand=True, fill='both')
-            
-    def add_prefixes(self, query: str) -> str:
-        return '\n'.join(self.PREFIXES) + query
         
-
+    def create_results_text(self, data: list[list]) -> None:
+        result_win = tk.Tk()
+        result_win.title("Результаты запроса")
+        
+        result_txt = st.ScrolledText(result_win)
+        result_txt.pack(expand=True, fill='both')
+        result_txt.insert(
+            '1.0', 
+            "\n".join(
+                (" | ".join(map(str, row)) for row in data)
+            )
+        )
+        
     def run(self) -> None:
         self.root.mainloop()
     
-            
 
 if __name__ == "__main__":
     App().run()
